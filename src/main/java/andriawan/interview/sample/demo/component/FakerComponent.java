2df21d0fad6899c6331c2424ecd5e68a95e96ea5
Muhammad Irwan Andriawan <irwan@andriawan.com>
2024-03-13 15:08:08 +0000

    chore(deps): add faker lib

---
+++ src/main/java/andriawan/interview/sample/demo/component/FakerComponent.java
@@ -0,0 +1,7 @@
+package andriawan.interview.sample.demo.component;
+
+import net.datafaker.Faker;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FakerComponent extends Faker {}
